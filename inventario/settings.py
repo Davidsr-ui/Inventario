90a9ff3e0e0f66b2e813ee03a9ba1b98f338d409
Davidsr-ui <danieldavid11sr@gmail.com>
2026-05-06 13:50:14 -0500

    Fix STATIC_URL setting

--- inventario/settings.py
+++ inventario/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 
-STSTATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
