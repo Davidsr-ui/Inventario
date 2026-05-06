be5e163105a21e917278719e76257e2881281a19
Davidsr-ui <danieldavid11sr@gmail.com>
2026-05-06 15:24:18 -0500

    Final Cloudinary configuration

--- inventario/settings.py
+++ inventario/settings.py
@@ -133,7 +133,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
